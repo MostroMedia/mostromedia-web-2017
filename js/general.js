330e8af2ed07b32c73302976e06f6ebe64da80b1
Camilo Arguello <camilo.arguello.rincon@gmail.com>
2017-03-26 10:57:40 -0500

    Basic structure by @danielpipe2203

--- js/general.js
+++ js/general.js
@@ -2,22 +2,9 @@
 
 // Initialize collapse button
 $(".button-collapse").sideNav({
-	/* var w = $(window).width();   Se debe detectar el tamaño del movil para activar la opción de 
-		if(w < 768){				closeOnClick: true : de lo contrario el menu no se cerrara al dar clic para navegar
-			menuWidth: 200, // Default is 300
-		    edge: 'left', // Choose the horizontal origin
-		    closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-		    draggable: true // Choose whether you can drag to open on touch screens
-		}
-		else{
-			menuWidth: 200, // Default is 300
-		    edge: 'left', // Choose the horizontal origin
-		    //closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-		    draggable: true // Choose whether you can drag to open on touch screens
-		} */
-		menuWidth: 200, // Default is 300
+		menuWidth: 100, // Default is 300
 		edge: 'left', // Choose the horizontal origin
-		//closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+		closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
 		draggable: true // Choose whether you can drag to open on touch screens
 	});
 // Initialize collapsible   
