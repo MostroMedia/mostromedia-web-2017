5cba85ba47942f0ce3ebe89e37ba4bff0d28c2c7
Camilo Arguello <camilo.arguello.rincon@gmail.com>
2017-03-28 17:51:29 -0500

    adding background particles on home section 80%

--- js/index.js
+++ js/index.js
@@ -28,6 +28,95 @@ class Work extends React.Component{
   }
 }
 
+let scene, camera, renderer, particles, saturn;
+let width = window.innerWidth, height = window.innerHeight;
+
+const colores = [0xE8540D, 0xCCCCCC, 0xE8540D];
+
+Init();
+animate();
+
+function Init(){
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+  camera.lookAt(scene.position);
+  camera.position.z = 500;
+
+  renderer = new THREE.WebGLRenderer();
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(width, height);
+  renderer.setClearColor(0x363636);
+  renderer.shadowMap.enabled = true;
+
+  const ambientLight = new THREE.AmbientLight();
+  scene.add(ambientLight);
+
+  const light = new THREE.DirectionalLight();
+  light.position.set(200,200,200);
+  light.castShadow = true;
+  light.shadow.camera.left = -100;
+  light.shadow.camera.right = 100;
+  light.shadow.camera. top = 100;
+  light.shadow.camera.bottom = -100;
+  scene.add(light);
+
+  drawParticles();
+  drawSaturn();
+
+  document.body.appendChild(renderer.domElement);
+
+  window.addEventListener('resize', onResize);
+  return renderer;
+}
+
+
+
+function onResize(){
+  width = window.innerWidth;
+  height = window.innerHeight;
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(width,height);
+}
+
+function animate(){
+  requestAnimationFrame(animate);
+  render();
+}
+
+function render(){
+  particles.rotation.x += 0.001;
+  particles.rotation.y -= 0.004;
+  saturn.rotation.y += 0.003;
+  renderer.render(scene, camera);
+}
+
+function drawParticles(){
+  particles = new THREE.Group();
+  scene.add(particles);
+  const geometry =new THREE.TetrahedronGeometry(5,0);
+  for(let i=0; i<100; i++){
+    const material = new THREE.MeshPhongMaterial({
+      color : colores[Math.floor(Math.random() * colores.length)],
+      shading: THREE.FlatShading
+    });
+
+    const mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set((Math.random() - 0.5) * 1000,
+                      (Math.random() - 0.5) * 1000,
+                      (Math.random() - 0.5) * 1000);
+    mesh.updateMatrix();
+    mesh.matrixAutoUpdate = false;
+    particles.add(mesh);
+  }
+}
+
+function drawSaturn(){
+  saturn = new THREE.Group();
+}
+
+
+
 class HomeContent extends React.Component{
   render(){
     return(
