5f33df643773237d2abdf5813b873322c7cedd9a
Camilo Arguello <camilo.arguello.rincon@gmail.com>
2017-04-13 23:12:06 -0500

    adding team section with photos, still it has a carousel functionality

--- js/index.js
+++ js/index.js
@@ -51,17 +51,92 @@ class Menu extends React.Component{
   }
 }
 
-
-
 // ************************************************************************************************************
 // Seccion CONTACT
 // ************************************************************************************************************
 
+const TextoContact = [
+  { titulo: '¿Quieres saber más?, conversemos'},
+  { call: 'Llámanos', number: 3142588148}
+]
+
+class FormularioContact extends React.Component{
+  render(){
+    return(
+      <form>
+        <div className="input-field">
+          <i className="material-icons prefix">account_circle</i>
+          <input id="icon_prefix" type="text" className="validate" />
+          <label>Your Name</label>
+        </div>
+
+        <div className="input-field">
+          <i className="white-text material-icons prefix">mode_edit</i>
+          <input id="email" type="email" className="validate" />
+          <label>Email</label>
+        </div>
+
+        <div className="input-field">
+        <i className="white-text material-icons prefix">message</i>
+        <textarea id="textarea" className="materialize-textarea"></textarea>
+        <label>Message</label>
+        </div>
+
+        <button className="white black-text btn waves-effect waves-light" type="submit" name="action">Submit
+          <i className="material-icons right">send</i>
+        </button>
+      </form>
+    )
+  }
+}
+
+class ContentContact extends React.Component{
+  render(){
+    return(
+      <div>
+        <div id="content-contact" className="col s12 m5">
+          <div className="valign-wrapper">
+            <h4 className="valign">{this.props.call} <br /> {this.props.number}</h4>
+          </div>
+        </div>
+        <div id="form-contact" className="col s12 m5">
+          <div className="valign-wrapper">
+            <FormularioContact />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+class TitleContact extends React.Component{
+  render(){
+    return(
+        <div className="center">
+          <h1>{this.props.titulo}</h1>
+          <div className="progress">
+              <div className="determinate"></div>
+          </div>
+        </div>
+    )
+  }
+}
+
 class Contact extends React.Component{
   render(){
     return(
-      <section>
-        <h1>Contact</h1>
+      <section id="contact">
+        <div className="container">
+          <div className="row">
+            <TitleContact
+              titulo={this.props.textContact[0].titulo}
+            />
+            <ContentContact
+              call={this.props.textContact[1].call}
+              number={this.props.textContact[1].number}
+            />
+          </div>
+        </div>
       </section>
     );
   }
@@ -72,17 +147,115 @@ class Contact extends React.Component{
 // Seccion TEAM
 // ************************************************************************************************************
 
+const MOSTROTEAM = [
+  {
+    name: 'Cristhian Cepeda',
+    img: 'img/mostros/cristhian_cepeda.jpg',
+    alt: 'Imagen de Cristhian Cepeda - Mostro Team',
+    position: '',
+    quote: 'Makia',
+    color: '#2ECC71'
+  },
+  {
+    name: 'Camilo Arguello',
+    img: 'img/mostros/camilo_arguello.jpg',
+    alt: 'Imagen de Camilo Arguello - Mostro Team',
+    position: 'Developer',
+    quote: 'Ingenio',
+    color: '#FFCC08'
+  },
+  {
+    name: 'Juan Garay',
+    img: 'img/mostros/juan_garay.jpg',
+    alt: 'Imagen de Juancho Garay - Mostro Team',
+    position: '',
+    quote: 'Confianza',
+    color: '#3498DB'
+  },
+  {
+    name: 'Sebastian Díaz',
+    img: 'img/mostros/sebastian_diaz.jpg',
+    alt: 'Imagen de Sebastian Díaz - Mostro Team',
+    position: '',
+    quote: 'Compromiso',
+    color: '#BDC3C7'
+  },
+  {
+    name: 'Daniela Sanchez',
+    img: 'img/mostros/daniela_sanchez.jpg',
+    alt: 'Imagen de Daniela Sanchez - Mostro Team',
+    position: '',
+    quote: 'Creatividad',
+    color: '#FA7878'
+  },
+  {
+    name: 'Daniel Valenzuela',
+    img: 'img/mostros/daniel_valenzuela.jpg',
+    alt: 'Imagen de Daniel Valenzuela - Mostro Team',
+    position: '',
+    quote: 'Empatía',
+    color: '#8E44AD'
+  },
+
+]
+
+class ItemTeam extends React.Component{
+  componentDidMount(){
+    $('.carousel').carousel();
+  }
+  render(){
+    return(
+      <li className="carousel-item">
+        <img src={this.props.img} alt={this.props.alt} />
+        <h3>{this.props.name}</h3>
+        <h4 style={{ color: this.props.color }}>{this.props.quote}</h4>
+      </li>
+    )
+  }
+}
 
 class Team extends React.Component{
   render(){
+    console.log(this.props)
+    let mostroTeam = []
+    this.props.mostros.forEach((mostro) => {
+      mostroTeam.push(
+        <ItemTeam
+          name={mostro.name}
+          key={mostro.name}
+          img={mostro.img}
+          alt={mostro.alt}
+          quote={mostro.quote}
+          color={mostro.color}
+        />
+      )
+    })
     return(
-      <section>
-        <h1>Team</h1>
+      <section id="team">
+        <ul className="carousel">
+          {mostroTeam}
+        </ul>
       </section>
     );
   }
 }
 
+// ************************************************************************************************************
+// Seccion VIDEO
+// ************************************************************************************************************
+
+class VideoBackground extends React.Component{
+  render(){
+    return(
+      <section id="video" className="fullscreen-bg">
+        <video id="myVideo" className="fullscreen-bg__video" muted autoPlay loop>
+          <source src="video/videoBG.mp4" type="video/mp4" />
+            Este navegador no soporta reproduccion de <code>video</code>
+        </video>
+      </section>
+    )
+  }
+}
 
 // ************************************************************************************************************
 // Seccion WORK
@@ -139,12 +312,9 @@ class PairItemsWork extends React.Component{
 
 class TxtWork extends React.Component{
   render(){
-    console.log(this.props.clase)
     return(
       <div className={this.props.clase}>
-        <h1>
-          Lo que nos gusta hacer
-        </h1>
+        <h1>Lo que nos gusta hacer</h1>
         <div className="divider"></div>
       </div>
     )
@@ -157,8 +327,8 @@ class Work extends React.Component{
       <section id="work" className="container">
         <div className="row">
           <div id="serv-section" className="col s12">
-            <TxtWork clase="col s4" />
-            <PairItemsWork whatwelove={WHATWELOVE} clase="col s8"/>
+            <TxtWork clase="col s12 m4" />
+            <PairItemsWork whatwelove={WHATWELOVE} clase="col s12 m8"/>
           </div>
         </div>
       </section>
@@ -276,14 +446,6 @@ class ParticlesHome extends React.Component{
   }
 }
 
-class BotonComencemos extends React.Component{
-  render(){
-    return(
-      <button className="btn-large right grey darken-4">Comencemos</button>
-    )
-  }
-}
-
 class HomeContent extends React.Component{
   render(){
     return(
@@ -324,7 +486,7 @@ class IconHome extends React.Component{
       )
     })
     return(
-      <ul id="iconos-home">{myIcon}</ul>
+      <ul id="iconos-home" className="hide-on-med-and-down">{myIcon}</ul>
     )
   }
 }
@@ -335,7 +497,6 @@ class Home extends React.Component{
       <section id="home" className="container-fluid">
         <HomeContent />
         <IconHome iconos={ICONOS} />
-        <BotonComencemos />
       </section>
     )
   }
@@ -351,8 +512,9 @@ class Main extends React.Component{
       <div>
         <Home />
         <Work />
-        <Team />
-        <Contact />
+        <VideoBackground />
+        <Team mostros={MOSTROTEAM}/>
+        <Contact textContact={TextoContact}/>
         <ParticlesHome />
         <Menu itemsMenu={MiMenu}/>
       </div>
