d383345eae838595e15f14949f84f915e67e394b
Camilo Arguello <camilo.arguello.rincon@gmail.com>
2017-05-16 05:51:52 -0500

    updating styles on team section: align vertical and border

--- js/index.js
+++ js/index.js
@@ -290,10 +290,10 @@ class Team extends React.Component{
     $('.listMostro').slick({
       centerMode: true,
       centerPadding: '60px',
-      slidesToShow: 3,
+      slidesToShow: 5,
       responsive: [
         {
-          breakpoint: 768,
+          breakpoint: 800,
           settings: {
             arrows: false,
             centerMode: true,
