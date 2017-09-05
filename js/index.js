3aeb0a36f3d4617e783ad37596b9a55cc17c03dd
Camilo Arguello <camilo.arguello.rincon@gmail.com>
2017-09-04 22:35:22 -0500

    fixing title size on mobile

--- js/index.js
+++ js/index.js
@@ -400,7 +400,7 @@ class TitleContact extends React.Component{
 	render(){
 		return(
 			<div className="center">
-				<h1>{this.props.titulo}</h1>
+				<h1 id="title_style_h1">{this.props.titulo}</h1>
 				<div className="progress">
 					<div className="determinate"></div>
 				</div>
