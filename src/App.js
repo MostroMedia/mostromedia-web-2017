0b4ff210496026ff39067a2184aac01c21b03703
Camilo Arguello <camilo.arguello.rincon@gmail.com>
2017-09-09 12:46:13 -0500

    change info and create build package

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import * as firebase from 'firebase'
+import logo from './logo.png'
+import './App.css'
+
+const config = {
+    apiKey: "AIzaSyClXRieTtf4Bc5M-JwzsCfWjDb5_yD9YSw",
+    authDomain: "mostromedia2013.firebaseapp.com",
+    databaseURL: "https://mostromedia2013.firebaseio.com",
+    projectId: "mostromedia2013",
+    storageBucket: "mostromedia2013.appspot.com",
+    messagingSenderId: "764454126810"
+  };
+  firebase.initializeApp(config);
 
 class App extends Component {
   render() {
@@ -8,10 +19,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Mostro Media</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Mostro is updating. Thanks for your patiente.
         </p>
       </div>
     );
